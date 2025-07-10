2689a05ee5e459a94e874ac1365b08595f36687e
nlaw39 <nicholaslaw201@gmail.com>
2025-07-10 11:23:03 +0900

    project start

---
+++ Cafe Crumble/Assets/Scripts/BaseUnitScript.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class BaseUnitScript : MonoBehaviour
+{
+    public int baseHealthPoints = 5;
+    public int baseAttackDamage = 2;
+
+    public int currentHealthPoints = baseHealthPoints;
+    public int currentAttackDamage = baseAttackDamage;
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
