9b59680a89baf367adf37a7bad16f1b44fc4216e
nlaw39 <nicholaslaw201@gmail.com>
2025-07-11 12:01:16 +0900

    not really working text boxes

--- Cafe Crumble/Assets/Scripts/Unit Scripts/BaseUnitScript.cs
+++ Cafe Crumble/Assets/Scripts/Unit Scripts/BaseUnitScript.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using TMPro;
+using System.Diagnostics;
 
 public class BaseUnitScript : MonoBehaviour
 {
@@ -8,17 +10,38 @@ public class BaseUnitScript : MonoBehaviour
     public int currentHealthPoints;
     public int currentAttackDamage;
 
+    public TMP_Text healthText;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        UnityEngine.Debug.Log("I'm starting");
         currentHealthPoints = baseHealthPoints;
         currentAttackDamage = baseAttackDamage;
-}
+
+        UnityEngine.Debug.Log("Trying to get Health Text Box");
+        healthText = GetComponent<TMP_Text>();
+        if (healthText != null)
+        {
+            UnityEngine.Debug.Log("Successfully got the text box");
+        }
+        UpdateHealth();
+    }
+
+    void Initialize()
+    {
+        
+    }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    void UpdateHealth()
+    {
+        healthText.text = "" + currentHealthPoints;
     }
 }
