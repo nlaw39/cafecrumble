e17ffde75ee23f3dd90b07e8e6768c9b7c3e1961
nlaw39 <nicholaslaw201@gmail.com>
2025-07-11 09:33:00 +0900

    fixed gitignore and base unit script error

--- Cafe Crumble/Assets/Scripts/BaseUnitScript.cs
+++ Cafe Crumble/Assets/Scripts/BaseUnitScript.cs
@@ -5,15 +5,16 @@ public class BaseUnitScript : MonoBehaviour
     public int baseHealthPoints = 5;
     public int baseAttackDamage = 2;
 
-    public int currentHealthPoints = baseHealthPoints;
-    public int currentAttackDamage = baseAttackDamage;
+    public int currentHealthPoints;
+    public int currentAttackDamage;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
-    }
+        currentHealthPoints = baseHealthPoints;
+        currentAttackDamage = baseAttackDamage;
+}
 
     // Update is called once per frame
     void Update()
